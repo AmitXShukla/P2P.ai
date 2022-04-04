1070fdc89629654a97f6fd3216eb745451bd3072
Amit X. Shukla <Amit.X.Shukla@kp.org>
2022-04-04 11:04:06 -0700

    v0.0.9

--- src/P2P.jl/docs/make.jl
+++ src/P2P.jl/docs/make.jl
@@ -3,22 +3,30 @@ using Documenter, P2P
 
 makedocs(sitename="P2P.ai",
     pages=[
-        "Introduction" => "introduction.md"
-        "Tutorials" => [
-            "About GL" => "tutorials/aboutgl.md"
-            "GL Processes" => "tutorials/glprocesses.md"
-            "ERD" => "tutorials/erd.md"
-            "Installing Julia" => "tutorials/installation.md"
-            "ELT vs ETL" => "tutorials/elt.md"
-            "Self-Service Data Analytic" => "tutorials/selfservice.md"
-            "Visualizations, Buttons, sliders, filters, n-D plots, plots vs graphs" => "tutorials/plots.md"
-            "p-value, null hypothesis and real time analytic" => "tutorials/analytic.md"
-            "Time Series, Impact analysis" => "tutorials/timeseries.md"
-            "Basics of ML" => "tutorials/mlbasics.md"
-            "ML for GL" => "tutorials/mlforgl.md"
-            "NLP for GL" => "tutorials/nlp.md"
-            "Graph Theory / Network Science" => "tutorials/graph.md"
-        ]
+        "Introduction" => "index.md"
+        "Define Problem" => "define.md"
+        "How did it get here" => "path.md"
+        "what do we know" => "process.md"
+        "get Facts straight" => "data.md"
+        "would could should" => "analysis.md"
+        "find solution" => "solve.md"
+        "UAT" => "test.md"
+        "retrain" => "retrain.md"
+        # "Tutorials" => [
+        #     "About GL" => "tutorials/aboutgl.md"
+        #     "GL Processes" => "tutorials/glprocesses.md"
+        #     "ERD" => "tutorials/erd.md"
+        #     "Installing Julia" => "tutorials/installation.md"
+        #     "ELT vs ETL" => "tutorials/elt.md"
+        #     "Self-Service Data Analytic" => "tutorials/selfservice.md"
+        #     "Visualizations, Buttons, sliders, filters, n-D plots, plots vs graphs" => "tutorials/plots.md"
+        #     "p-value, null hypothesis and real time analytic" => "tutorials/analytic.md"
+        #     "Time Series, Impact analysis" => "tutorials/timeseries.md"
+        #     "Basics of ML" => "tutorials/mlbasics.md"
+        #     "ML for GL" => "tutorials/mlforgl.md"
+        #     "NLP for GL" => "tutorials/nlp.md"
+        #     "Graph Theory / Network Science" => "tutorials/graph.md"
+        # ]
         "API" => "api.md"
     ],
 )
